e0c09c6ae2a395e96e8a4533536032bd63f384e9
Paulo Santos <paulojunke@hotmail.com>
2023-05-23 13:22:26 +0100

    part2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = ({persons, filter}) => {
   return (
@@ -44,15 +45,20 @@ const PersonForm = ({ newName, newNumber, updateName, updateNumber, saveContact 
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then((event) => {
+        console.log(event.data);
+        setPersons(event.data); 
+      })
+  }, [])
 
   const updateName = (event) => {
     event.preventDefault();
