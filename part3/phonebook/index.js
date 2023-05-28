0b36ec43d4b97af1ba84306911f16bf460c6a9db
Paulo Santos <paulojunke@hotmail.com>
2023-05-28 18:13:48 +0100

    part3.5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -26,6 +26,10 @@ let phonebook = [
     }
 ];
 
+const generateId = () => {
+    return Math.floor((Math.random() * 1_000_000));
+  }
+
 app.get('/api/persons', (request, response) => {
     response.send(phonebook);
 })
@@ -40,6 +44,21 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).send();
     }
 })
+
+app.post('/api/persons', (request, response) => {
+
+    if (!request.body.name || !request.body.number) {
+        return response.status(404).send();
+    }
+
+    const person = {
+        name: request.body.name,
+        number: request.body.number,
+        id: generateId()
+    }
+    phonebook = phonebook.concat(person);
+
+});
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id);
