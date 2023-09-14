f35836e370490740f7399c3b666217dce3c62515
Paulo <paulojunke@hotmail.com>
2023-09-14 18:55:46 +0100

    1.2

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,59 +1,42 @@
-import { useState } from 'react'
-
 const Header = (props) => {
-  return (
-    <h1>{props.text}</h1>
-  );
+  return <h1>{props.course}</h1>;
 }
 
+const Part = (props) => {
+  return <p>
+    {props.name} {props.number}
+  </p>
+}
+
+const Content = (props) => {
+  return <>
+    <Part name={props.name1} number={props.number1}></Part>
+    <Part name={props.name2} number={props.number2}></Part>
+    <Part name={props.name3} number={props.number3}></Part>
+  </>
+  
+}
+
+const Total = (props) => {
+  return <p>Number of exercises {props.number1 + props.number2 + props.number3}</p>;
+}
+
+
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0});
-  const [currentVotes, setCurrentVotes] = useState(0);
-
-  let currentBestAnecdote = parseInt(Object.keys(votes).reduce((a, b) => votes[a] > votes[b] ? a : b));
-  const [bestAnecdote, setBestAnecdote] = useState(currentBestAnecdote)
-
-
-  const changeAnecdotes = () => {
-    let next = Math.floor(Math.random() * anecdotes.length);
-    setCurrentVotes(votes[next]);
-    setSelected(next);
-  }
-
-  const voteAnecdote = () => {
-    let nextVotes = votes;
-    nextVotes[selected] += 1;
-    let currentBestAnecdote = parseInt(Object.keys(nextVotes).reduce((a, b) => nextVotes[a] > nextVotes[b] ? a : b));
-    setBestAnecdote(currentBestAnecdote);
-    setVotes(nextVotes);
-    setCurrentVotes(nextVotes[selected]);
-  }
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
-    <>
-      <Header text={"Anecdote of the day"}></Header>
-      <div>{anecdotes[selected]}</div> 
-      <p>This anecdote has {currentVotes} votes.</p>
-      <button onClick={voteAnecdote}>Vote</button>
-      <button onClick={changeAnecdotes}>Random</button>
-      <Header text={"Anecdote with most votes"}></Header>
-      <p>{anecdotes[bestAnecdote]}</p>
-      <p>This anecdote has {votes[bestAnecdote]} votes.</p>
-
-    </>
-
+    <div>
+      <Header course={course}></Header>
+      <Content name1={part1} number1={exercises1} name2={part2} number2={exercises2} name3={part3} number3={exercises3}></Content>
+      <Total number1={exercises1} number2={exercises2} number3={exercises3}></Total>
+    </div>
   )
 }
 
